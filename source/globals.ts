62892f0c83594391380bf6ada7033acee1322b44
sbrad93 <shannon.brady2@marist.edu>
2022-09-05 16:47:55 -0400

    resolved Issue #1 (9/5)

--- source/globals.ts
+++ source/globals.ts
@@ -11,8 +11,8 @@
 //
 // Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
 //
-const APP_NAME: string    = "TSOS";   // 'cause Bob and I were at a loss for a better name.
-const APP_VERSION: string = "0.07";   // What did you expect?
+const APP_NAME: string    = "chaOS";   // can characterize my thoughts while completing this project ('kay-aas') or maybe the cha cha slide ('chaa-oh-ess'), either is fitting
+const APP_VERSION: string = "3.1.4";   // 159265358979323846264338327950288419716, that's all I got
 
 const CPU_CLOCK_INTERVAL: number = 100;   // This is in ms (milliseconds) so 1000 = 1 second.
 
--- test/glados-ip0.js
+++ test/glados-ip0.js
@@ -9,17 +9,17 @@ function Glados() {
       var msg = "Hello [subject name here]. Let's test project ZERO.\n";
       msg += "Before we start, however, keep in mind that although fun and learning are our primary goals, serious injuries may occur. ";
       msg += "Cake, and grief counseling, will be available at the conclusion of the test.";
-      alert(msg);
+      //alert(msg);
    };
 
    this.afterStartup = function() {
 
-      // Test the 'help' command.
-      _KernelInputQueue.enqueue('h');
-      _KernelInputQueue.enqueue('e');
-      _KernelInputQueue.enqueue('l');
-      _KernelInputQueue.enqueue('p');
-      TSOS.Kernel.prototype.krnInterruptHandler(KEYBOARD_IRQ, [13, false]);
+      // // Test the 'help' command.
+      // _KernelInputQueue.enqueue('h');
+      // _KernelInputQueue.enqueue('e');
+      // _KernelInputQueue.enqueue('l');
+      // _KernelInputQueue.enqueue('p');
+      // TSOS.Kernel.prototype.krnInterruptHandler(KEYBOARD_IRQ, [13, false]);
       
       // Test the 'ver' command.
       _KernelInputQueue.enqueue('v');
